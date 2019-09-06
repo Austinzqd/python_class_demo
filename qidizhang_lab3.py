e8bbdc9f6da93619acf0d7b437945e1749e23823
Austinzqd <54647037+Austinzqd@users.noreply.github.com>
2019-09-06 12:30:13 -0400

    Delete qidizhang_lab3.py

--- qidizhang_lab3.py
+++
@@ -1,8 +0,0 @@
-a = 50
-b = 20
-c = 1
-d = (a*12)*(b*12)*c/231
-print("plot_length:" + str(a) + "ft")
-print("plot_width:" + str(b) + "ft")
-print("rainfall_inches:" + str(c)+ "inch")
-print("runoff_gallons:" + str(d)+"gallons")
